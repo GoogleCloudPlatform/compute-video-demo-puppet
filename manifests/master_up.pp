b28b2750f8c16b13fb7a332e1566cfdf207b8cc0
ashmrtnz <ashmrtnz@google.com>
2014-08-05 16:30:57 +0000

    Compatability changes

--- manifests/master_up.pp
+++ manifests/master_up.pp
@@ -1,11 +1,11 @@
-gce_instance { "ash-puppet-enterprise-master":
+gce_instance { 'puppet-enterprise-master':
     ensure       => present,
     description  => 'A PE Master, Console and PuppetDB',
     machine_type => 'n1-standard-1',
     zone         => 'us-central1-a',
     network      => 'default',
-    require      => Gce_disk['ash-puppet-enterprise-master'],
-    disk         => 'ash-puppet-enterprise-master',
+    require      => Gce_disk['puppet-enterprise-master'],
+    disk         => 'puppet-enterprise-master,boot',
     tags         => ['puppet', 'master'],
     startupscript        => 'puppet-enterprise.sh',
     metadata             => {
@@ -13,26 +13,48 @@ gce_instance { "ash-puppet-enterprise-master":
       'pe_version'       => '3.3.0',
       'pe_consoleadmin'  => 'admin@example.com',
       'pe_consolepwd'    => 'puppetenterprise',
-    },
-    modules      => ['puppetlabs-apache', 'puppetlabs-firewall', 'puppetlabs-stdlib', 'puppetlabs-vcsrepo', 'puppetlabs-gce_compute'],
-    manifest     => '
-      class puppet-enterprise-master {
+      'setup'            => '
         package { "git":
-          ensure  => present,
+          ensure => present,
         }
 
         vcsrepo { "/opt/compute-video-demo-puppet":
-          ensure    => present,
-          provider  => git,
-          source    => "https://github.com/GoogleCloudPlatform/compute-video-demo-puppet.git",
-          require   => Package["git"],
+          ensure   => present,
+          provider => git,
+          source   => "https://github.com/GoogleCloudPlatform/compute-video-demo-puppet.git",
+          require  => Package["git"],
         }
+
+        file { "/etc/puppetlabs/puppet/autosign.conf":
+          ensure  => file,
+          content => "*.${domain}",
+        }
+
+        file { "/etc/puppetlabs/puppet/manifests/site.pp":
+          ensure => file,
+          content => "node /^puppet-agent-\\d+/ {
+          class { \"apache\": }
+
+          include apache::mod::headers
+
+          file {\"/var/www/index.html\":
+            ensure  => present,
+            content => template(\"/opt/compute-video-demo-puppet/index.html.erb\"),
+            require => Class[\"apache\"],
+          }
+        }"
       }
 
-      include puppet-enterprise-master'
+      firewall { "100 allow 443 and 8140":
+        port   => [8140, 443],
+        proto  => tcp,
+        action => accept,
+      }' 
+    },
+    modules      => ['puppetlabs-apache', 'puppetlabs-firewall', 'puppetlabs-stdlib', 'puppetlabs-vcsrepo', 'puppetlabs-gce_compute'],
 }
 
-gce_disk { "ash-puppet-enterprise-master":
+gce_disk { "puppet-enterprise-master":
   ensure        => present,
   source_image  => 'centos-6',
   zone          => 'us-central1-a',
